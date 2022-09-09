aaf39558fbb677b040e1d832710dc3fb15d50b67
maz004 <97961745+maz004@users.noreply.github.com>
2022-09-09 10:44:35 +0100

    Update Gouvernance.js

--- src/pages/Gouvernance.js
+++ src/pages/Gouvernance.js
@@ -10,7 +10,7 @@ class Gouvernance extends React.Component {
 
     render(){
         return <>
-                <iframe title="HomePage" width="1400" height="670" src="https://app.powerbi.com/reportEmbed?reportId=601f53f9-ca85-4a3d-b80a-96e8f31d6547&autoAuth=true&ctid=3dce8bf6-d374-4bbf-92c0-aafa67adb68e" frameborder="0" allowFullScreen="true"></iframe>        </>
+                <iframe title="HomePage" width="1400" height="670"  src="https://app.powerbi.com/reportEmbed?reportId=601f53f9-ca85-4a3d-b80a-96e8f31d6547&autoAuth=true&ctid=3dce8bf6-d374-4bbf-92c0-aafa67adb68e" frameborder="0" allowFullScreen="true"></iframe>        </>
     }
 }
 
